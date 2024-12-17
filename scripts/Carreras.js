324c51a446f802cd8259d9ae98983c828ed0fa9e
sofi <sofiaencina040704@gmail.com>
2024-12-17 18:11:39 -0300

    cambio disposicion de las carreras a grid

--- scripts/Carreras.js
+++ scripts/Carreras.js
@@ -17,30 +17,30 @@ fetch('http://127.0.0.1:3000/api/v1/carreras')
 
     carreras.forEach(carrera => {
         let div = document.createElement('div');
-        div.className = "box";
+        div.className = "cell box has-background-warning is-col-min-10";
         
         let id = document.createElement('h2');
-        id.className = "subtitle";
-        id.textContent = carrera.id;
+        id.className = "subtitle has-text-black";
+        id.textContent = carrera.id_carrera;
 
         let nombre = document.createElement('h2');
-        nombre.className = "subtitle";
+        nombre.className = "subtitle has-text-black";
         nombre.textContent = `Nombre: ${carrera.nombre_carrera}`;
 
         let pais_sede = document.createElement('h2');
-        pais_sede.className = "subtitle";
+        pais_sede.className = "subtitle has-text-black";
         pais_sede.textContent = `Sede: ${carrera.pais_sede}`;
 
         let anio = document.createElement('h2');
-        anio.className = "subtitle";
+        anio.className = "subtitle has-text-black";
         anio.textContent = `Año: ${carrera.anio}`;
 
         let primer_puesto = document.createElement('h2');
-        primer_puesto.className = "subtitle";
+        primer_puesto.className = "subtitle has-text-black";
         primer_puesto.textContent = `Piloto ganador: ${carrera.piloto ? carrera.piloto.nombre_piloto : 'desconocido'}`;
 
         let circuito_asociado = document.createElement('h2');
-        circuito_asociado.className = "subtitle";
+        circuito_asociado.className = "subtitle has-text-black";
         circuito_asociado.textContent = `Circuito: ${carrera.circuito ? carrera.circuito.nombre : 'Desconocido'}`;
 
         div.appendChild(id);
